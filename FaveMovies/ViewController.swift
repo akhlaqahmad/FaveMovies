02fd0f754476dffe928911b4f123ec5fc82e12ec
Akhlaq Ahmad <akhlaqahmadofficial@gmail.com>
2020-11-01 09:15:48 +0800

    Initial Commit

---
+++ FaveMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaveMovies
+//
+//  Created by Ahmad on 01/11/2020.
+//  Copyright © 2020 Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
